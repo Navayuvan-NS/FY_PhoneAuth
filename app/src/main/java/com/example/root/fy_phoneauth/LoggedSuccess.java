f068993753d5cc8d8d90df8cc7ff0aafda860913
Navayuvan-NS <ns.navayuvan@gmail.com>
2019-02-22 18:12:55 +0530

    22.02.2019

--- app/src/main/java/com/example/root/fy_phoneauth/LoggedSuccess.java
+++ app/src/main/java/com/example/root/fy_phoneauth/LoggedSuccess.java
@@ -1,7 +1,11 @@
 package com.example.root.fy_phoneauth;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoggedSuccess extends AppCompatActivity {
 
@@ -9,6 +13,13 @@ public class LoggedSuccess extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logged_success);
+
+    }
+
+    public void logoutBtn(View view) {
+
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(LoggedSuccess.this, PhoneAuth.class));
 
     }
 }
